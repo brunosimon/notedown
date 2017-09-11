54901baf5dc93033a1ba257af4bd550137dc0f8f
brunosimon <simon.bruno.77@gmail.com>
2017-09-12 01:22:45 +0200

    :sparkles: Add syntax coloration with custom style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import CodeMirror from 'codemirror'
 import 'codemirror/addon/mode/simple.js'
+import 'codemirror/addon/scroll/simplescrollbars.js'
+import 'codemirror/addon/scroll/simplescrollbars.css'
 import 'codemirror/lib/codemirror.css'
 
 import './style.styl'
@@ -7,7 +9,37 @@ import './style.styl'
 
 const $textarea = document.createElement('textarea')
 $textarea.innerHTML = `
-    test
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur ver. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam maiores aperiam pariatur explicabo odio dicta culpa perspiciatis aliquid nihil sapiente labore asperiores, exercitationem possimus esse blanditiis, quas repellendus non voluptate!
+Omagni sunt exercitationem, rem accusamus quidem dolor
+Reprehenderit repellendus perferendis nam a. Delectus, commodi illum quas
+
+Explicabo veniam, perspiciatis.
+
+
+# This is a list
+- Lorem
+- Ipsum (with parentheses)
+- Dolor
+- Site amet [Something inside brackets]
+- And that "a string"
+
+# This is an indented list
+- Lorem
+    - Ipsum
+    - Dolor
+
+# This is a todo list
+- [ ] Clean house
+- [x] Buy stuff
+- [ ] Do homework
+- [!] Call mum
+- [√] Feed dog
+- [ ] Cool project
+    - [√] Design
+    - [?] Website
+    - [ ] Application
+
+# Hey look, it’s a folded title
 `
 document.body.appendChild($textarea)
 
@@ -17,30 +49,38 @@ CodeMirror.defineSimpleMode(
         // The start state contains the rules that are intially used
         start:
         [
-            { regex: /test/, token: 'test2' },
-            // The regex matches the token, the token property contains the type
-            { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string' },
-            // You can match multiple tokens at once. Note that the captured
-            // groups must span the whole string in this case
-            { regex: /(function)(\s+)([a-z$][\w$]*)/, token: ['keyword', null, 'variable-2'] },
-            // Rules are matched in the order in which they appear, so there is
-            // no ambiguity between this one and the one above
-            { regex: /(?:function|var|return|if|for|while|else|do|this)\b/, token: 'keyword' },
-            { regex: /true|false|null|undefined/, token: 'atom' },
-            { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: 'number' },
-            { regex: /\/\/.*/, token: 'comment' },
-            { regex: /\/(?:[^\\]|\\.)*?\//, token: 'variable-3' },
-            // A next property will cause the mode to move to a different state
-            { regex: /\/\*/, token: 'comment', next: 'comment' },
-            { regex: /[-+\/*=<>!]+/, token: 'operator' },
-            // indent and dedent properties guide autoindentation
-            { regex: /[\{\[\(]/, indent: true },
-            { regex: /[\}\]\)]/, dedent: true },
-            { regex: /[a-z$][\w$]*/, token: 'variable' },
-            // You can embed other modes with the mode property. This rule
-            // causes all code between << and >> to be highlighted with the XML
-            // mode.
-            { regex: /<</, token: "meta", mode: { spec: 'xml', end: />>/ } }
+            { regex: /\s*#.+/, sol: true, token: 'title' },
+            { regex: /\(.+\)/, token: 'parenthesis' },
+            { regex: /(\[)([√xX])(])(\s)(.+)/, token: [null, 'checked', null, null, 'checked-value'] },
+            { regex: /(\[)([!])(])(\s)(.+)/, token: [null, 'danger', null, null, 'danger-value'] },
+            { regex: /(\[)([?])(])(\s)(.+)/, token: [null, 'warning', null, null, 'warning-value'] },
+            { regex: /\[\s]/, token: '' },
+            { regex: /\[.+\]/, token: 'brackets' }
+
+            // { regex: /test/, token: 'test2' },
+            // // The regex matches the token, the token property contains the type
+            // { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string' },
+            // // You can match multiple tokens at once. Note that the captured
+            // // groups must span the whole string in this case
+            // { regex: /(function)(\s+)([a-z$][\w$]*)/, token: ['keyword', null, 'variable-2'] },
+            // // Rules are matched in the order in which they appear, so there is
+            // // no ambiguity between this one and the one above
+            // { regex: /(?:function|var|return|if|for|while|else|do|this)\b/, token: 'keyword' },
+            // { regex: /true|false|null|undefined/, token: 'atom' },
+            // { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: 'number' },
+            // { regex: /\/\/.*/, token: 'comment' },
+            // { regex: /\/(?:[^\\]|\\.)*?\//, token: 'variable-3' },
+            // // A next property will cause the mode to move to a different state
+            // { regex: /\/\*/, token: 'comment', next: 'comment' },
+            // { regex: /[-+\/*=<>!]+/, token: 'operator' },
+            // // indent and dedent properties guide autoindentation
+            // { regex: /[\{\[\(]/, indent: true },
+            // { regex: /[\}\]\)]/, dedent: true },
+            // { regex: /[a-z$][\w$]*/, token: 'variable' },
+            // // You can embed other modes with the mode property. This rule
+            // // causes all code between << and >> to be highlighted with the XML
+            // // mode.
+            // { regex: /<</, token: "meta", mode: { spec: 'xml', end: />>/ } }
         ],
         // The multi-line comment state.
         comment: [
@@ -60,4 +100,11 @@ CodeMirror.defineSimpleMode(
 )
 
 
-const editor = CodeMirror.fromTextArea($textarea)
+CodeMirror.fromTextArea(
+    $textarea,
+    {
+        scrollbarStyle: 'simple',
+        lineWrapping: true,
+        indentUnit: 4
+    }
+)
